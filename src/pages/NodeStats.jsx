0cafbc845df069ab2c68c28ef0d8084052ff67c3
Dardeos <87314938+Dardeos@users.noreply.github.com>
2026-04-08 13:31:01 +0200

    Update NodeStats.jsx

--- src/pages/NodeStats.jsx
+++ src/pages/NodeStats.jsx
@@ -19,14 +19,13 @@ const NodeStats = () => {
 
     return (
         <div className="p-6 bg-brand-card rounded-xl border border-brand-border">
-            <h1 className="text-2xl font-bold text-indigo-500 mb-4">Analyse Node.js (Lab 8)</h1>
+            <h1 className="text-2xl font-bold text-500 mb-4">Analyse Node</h1>
             {loading ? (
-                <p>Chargement des données depuis Express...</p>
+                <p>Chargement des données</p>
             ) : (
                 <div className="space-y-4">
-                    <p className="text-lg">Voici les données traitées par le backend Node.js :</p>
+                    <p className="text-lg">Voici les données traitées par le backend Node :</p>
                     <div className="p-4 bg-black/10 rounded-lg font-mono">
-                        {/* Affichage brut pour tester */}
                         {JSON.stringify(stats, null, 2)}
                     </div>
                 </div>
